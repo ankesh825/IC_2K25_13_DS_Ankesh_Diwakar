4aace0299ce87c95b1945a655633bf60eb99d5b8
ankesh825 <ankeshdiwakar825@gmail.com>
2026-07-29 17:39:30 +0530

    Add basic C++ programs for output and array handling

---
+++ DSA_FIRST_LAB.cpp
@@ -0,0 +1,38 @@
+//Hello world program 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << " HELLO IIPS" << endl;
+    return 0;
+}
+
+//ARRAY SINGLE ELEMENT PRINT
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3] = {54, 55, 46};
+
+    cout << arr[1];
+
+    return 0;
+}
+
+// for loop by array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3] = {13, 44, 69};
+
+    for (int i = 0; i < 3; i++) {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
